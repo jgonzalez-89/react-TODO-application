33c8a7473fc9eca9a7b155e1ae6f75f6899af03d
jgonzalez-89 <kbcerverjo@gmail.com>
2022-12-13 20:19:39 +0100

    refactoring code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
-import Formulario from "./components/Formulario"
-import Header from "./components/Header"
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
 import ListadoTareas from "./components/ListadoTareas";
-
 
 function App() {
 
@@ -14,7 +12,70 @@ function App() {
   const eliminarTarea = (id) => {
     const tareasActualizadas = tareas.filter(tarea => tarea.id !== id)
     setTareas(tareasActualizadas)
+  }
 
+  // Actualizando --------------------------------------------------------------!
+  const [inputValue, setInputValue] = useState("");
+  const [list, setList] = useState([]);
+
+  // API urls
+  let host = 'https://assets.breatheco.de/apis/fake/todos/user/';
+  let user = 'jgonzalez89';
+
+  // fetch GET todos - consulta los todos desde la API
+  const GetTodos = async () => {
+    const url = host + user;
+    const request = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    const response = await fetch(url, request);
+    const responseJSON = await response.json();
+    responseJSON.map((item) => { setList((e) => [...e, item.label]); })
+  };
+
+
+  // fetch PUT todos - agrega un todo a la lista en la API
+  const PutTodos = async (todos) => {
+    const url = host + user;
+    const request = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todos),
+    };
+    const response = await fetch(url, request);
+    const responseJSON = await response.json();
+    if (responseJSON.ok) {
+      console.log("todo ok");
+    } else {
+      console.log('error', error);
+    }
+  }
+
+  // funcion onSubmit del form
+  const addTask = (event) => {
+    event.preventDefault();
+    if (inputValue === "") {
+      return;
+    } else {
+      // creo una array vacio, recorro list, y genero un array de objetos
+      var todosPUT = [];
+      for (let index = 0; index < list.length; index++) {
+        const element = list[index];
+        let item = {};
+        item['label'] = element;
+        item['done'] = false;
+        todosPUT.push(item);
+      };
+      // agrego la nueva tarea 'inputValue' en formato objeto
+      todosPUT.push({ 'label': inputValue, 'done': false });
+      // agrego el tecth para actualizar la API
+      fetchPutTodos(todosPUT);
+      setList([...list, inputValue]);
+      setInputValue("");
+    }
   }
 
   // const [posts, setPosts] = useState();
@@ -44,13 +105,13 @@ function App() {
   //        }
   //      })
   //      .then(resp => {
-   
+
   //          return resp.json();
   //      })
   //      .then(data => {
-           
+
   //          setPosts(data);
-           
+
   //      })
   //      .catch(error => {
   //          //Logs de errores
@@ -62,37 +123,31 @@ function App() {
   //     FetchApiPosts()
   //   }, [])
 
+  // function AgregarTarea() {
+  //   const tareaNueva = {label : "hola", done : true,}
 
-  //   function AgregarTarea() {
-  //     const tareaNueva = {label : "hola", done : true,}
+  //   const nuevoPost=[...posts, tareaNueva]
 
-  //     const nuevoPost=[...posts, tareaNueva]
- 
-  //     PutApiPosts(nuevoPost)
-  //   }
-  
-    
+  //   PutApiPosts(nuevoPost)
+  // }
 
   return (
     <div className="container mx-auto mt-20">
-      
-    <Header />
+
+      <Header />
       <div className="md:flex mt-12">
-      <Formulario
-        tareas={tareas}
-        setTareas={setTareas}
-        tareaObj={tareaObj}
-        setTarea={setTarea}
-      />
-      <ListadoTareas
-      tareas={tareas} //Agregar "posts" para ejecutar la Api
-      setTarea={setTarea}
-      eliminarTarea={eliminarTarea}
-      />
-      
+        <Formulario
+          tareas={tareas}
+          setTareas={setTareas}
+          tareaObj={tareaObj}
+          setTarea={setTarea}
+        />
+        <ListadoTareas
+          tareas={tareas} //Agregar "posts" para ejecutar la Api
+          setTarea={setTarea}
+          eliminarTarea={eliminarTarea}
+        />
       </div>
-      {/* <button onClick={AgregarTarea}>Click Aqui!</button> */}
-      
       <Footer />
     </div>
 
@@ -100,4 +155,3 @@ function App() {
 }
 
 export default App
-  
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,24 +2,23 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import Error from './Error';
 
-
-const Formulario = ({ tareas, setTareas, tareaObj, setTarea}) => {
+const Formulario = ({ tareas, setTareas, tareaObj, setTarea }) => {
   const [nombreTarea, setNombreTarea] = useState("");
-	const [nombre, setNombre] = useState("");
+  const [nombre, setNombre] = useState("");
   const [email, setEmail] = useState("");
   const [fecha, setFecha] = useState("");
   const [comentario, setComentario] = useState("");
   const [error, setError] = useState(false)
 
-  const generarId=()=>{
+  const generarId = () => {
     const fecha = Date.now().toString(36)
     const random = Math.random().toString(36).substr(2);
 
     return fecha + random
   }
 
-  useEffect(()=>{
-    if(Object.keys(tareaObj).length > 0 ){
+  useEffect(() => {
+    if (Object.keys(tareaObj).length > 0) {
       setNombre(tareaObj.nombre);
       setNombreTarea(tareaObj.nombreTarea);
       setEmail(tareaObj.email);
@@ -29,12 +28,11 @@ const Formulario = ({ tareas, setTareas, tareaObj, setTarea}) => {
     }
   }, [tareaObj])
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // Validacion de formulario
-    if([nombre, nombreTarea, email, fecha, comentario].includes("")){
+    if ([nombre, nombreTarea, email, fecha, comentario].includes("")) {
       console.log("Hay al menos un campo vacio")
       setError(true);
       return;
@@ -44,27 +42,26 @@ const Formulario = ({ tareas, setTareas, tareaObj, setTarea}) => {
     // Objeto de Tarea
     const objetoTareas = {
       nombre,
-      nombreTarea,  
-      email, 
-      fecha, 
+      nombreTarea,
+      email,
+      fecha,
       comentario,
 
     }
 
-    if(tareaObj.id) {
+    if (tareaObj.id) {
       // Editando el Registro de Tareas
       objetoTareas.id = tareaObj.id
-      const tareasActualizadas = tareas.map( tareaObjState => tareaObjState.id === tareaObj.id ? objetoTareas : tareaObjState )
+      const tareasActualizadas = tareas.map(tareaObjState => tareaObjState.id === tareaObj.id ? objetoTareas : tareaObjState)
 
       setTareas(tareasActualizadas)
       setTarea({})
 
-    }else {
+    } else {
       // Añadiendo Nueva Tarea al Registro
       objetoTareas.id = generarId();
       setTareas([...tareas, objetoTareas]);
     }
-
 
     // Reiniciar Formulario
     setNombre('')
@@ -82,9 +79,9 @@ const Formulario = ({ tareas, setTareas, tareaObj, setTarea}) => {
         <span className="text-sky-600 font-bold">Administralas</span>
       </p>
 
-      <form 
-      onSubmit={handleSubmit}
-      className="bg-white shadow-xl rounded-lg py-10 px-5 mb-10">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-xl rounded-lg py-10 px-5 mb-10">
         {error && <Error mensaje='Todos los campos son obligatorios' />}
         <div className='mb-5'>
           <label htmlFor='nombre' className='block text-gray-700 uppercase font-bold'>Nombre Persona</label>
@@ -151,7 +148,7 @@ const Formulario = ({ tareas, setTareas, tareaObj, setTarea}) => {
         <input
           type="submit"
           className="bg-sky-600 w-full p-3 text-white uppercase font-bold hover:bg-sky-800 rounded-md cursor-pointer transition-all"
-          value={tareaObj.id ? 'Editar Tarea' : 'Agregar Tarea' }
+          value={tareaObj.id ? 'Editar Tarea' : 'Agregar Tarea'}
 
         />
       </form>
