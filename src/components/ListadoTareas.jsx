3ec101e4f107e7849e75ee81e82b47bd31b0649e
jgonzalez-89 <kbcerverjo@gmail.com>
2022-12-13 20:20:23 +0100

    refactoring code

--- src/components/ListadoTareas.jsx
+++ src/components/ListadoTareas.jsx
@@ -28,7 +28,6 @@ const ListadoTareas = ({ tareas, setTarea, eliminarTarea }) => {
 
                 </>
             ) : (
-
                 <>
                     <h2 className='font-black text-3xl text-center'>No hay Tareas</h2>
                     <p className='text-lg mt-5 text-center mb-10'>
@@ -37,8 +36,6 @@ const ListadoTareas = ({ tareas, setTarea, eliminarTarea }) => {
                     </p>
                 </>
             )}
-
-
         </div>
     )
 }
